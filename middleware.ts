7d267afdc15c9d1ddee7978ea7241f459e801aaf
Muhamad Akbar Afriansyah <itsme@akbarafriansyah.my.id>
2024-06-05 03:03:56 +0700

    setup clerk

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { clerkMiddleware } from '@clerk/nextjs/server';
+
+export default clerkMiddleware({
+
+    publicRoutes: ['/', '/api/webhook/clerk'],
+
+    ignoredRoutes: ['/api/webhook/clerk']
+})
+
+
+export const config = {
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)']
+}
+
