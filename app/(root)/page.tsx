44c29a0f207b4dcbe897bda88aa3f07bf985987e
Muhamad Akbar Afriansyah <itsme@akbarafriansyah.my.id>
2024-06-10 17:47:00 +0700

    add ThreadCard for Home Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import ThreadCard from "@/components/cards/ThreadCard";
+import { fetchPosts } from "@/lib/actions/thread.actions";
+
+export default async function Home() {
+  const result = await fetchPosts(1, 30)
+
   return (
-    <div>
+    <>
       <h1 className="head-text text-left">Home</h1>
-    </div>
+      <section className="mt-9 flex flex-col gap-10">
+        {result.posts.map((idx) => {
+          return (
+            <ThreadCard 
+              id={idx.id}
+              text={idx.text}
+              author={idx.author}
+              community={idx.community}
+              createdAt={idx.createdAt}            
+            />
+        )})}
+      </section>
+    </>
   );
 }
